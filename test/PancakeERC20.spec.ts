3d2f63d87c6a68d057e3001119866cb8994e3a03
Nolann Biron <ponyswapfather@gmail.com>
2022-03-10 23:17:39 +0100

    init

--- test/PancakeERC20.spec.ts
+++ test/PancakeERC20.spec.ts
@@ -16,9 +16,11 @@ const TEST_AMOUNT = expandTo18Decimals(10)
 
 describe('PonyERC20', () => {
   const provider = new MockProvider({
-    hardfork: 'istanbul',
-    mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
-    gasLimit: 9999999
+    ganacheOptions:{
+      hardfork: 'istanbul',
+      mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
+      gasLimit: 9999999
+    }  
   })
   const [wallet, other] = provider.getWallets()
 
